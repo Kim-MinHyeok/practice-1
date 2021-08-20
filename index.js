b6be550bdfb5464512a272b8b4c32b68b147ec6a
Kim-MinHyeok <48408314+Kim-MinHyeok@users.noreply.github.com>
2021-08-20 17:06:47 +0900

    14강 로그아웃

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
 const config = require('./config/key')
-
 const { User } = require("./models/User");
+const { auth } = require('./middleware/auth');
 
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
@@ -18,9 +17,15 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+
+
+
 app.get('/',(req,res) => res.send('Hello world!'))
 
-app.post('/register', (req, res) => {
+
+
+
+app.post('/api/users/register', (req, res) => {
 
   const user = new User(req.body)
 
@@ -28,9 +33,12 @@ app.post('/register', (req, res) => {
     if(err) return res.json({ success: false, err})
     return res.status(200).json({ success: true })
   })
-}) 
+})
 
-app.post('/login', (req,res) => {
+
+
+
+app.post('/api/users/login', (req,res) => {
   User.findOne({ email: req.body.email }, (err, user) => {
     if(!user) {
       return res.json({
@@ -53,6 +61,37 @@ app.post('/login', (req,res) => {
     })
   })
 })
+
+
+
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({ _id:req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- models/User.js
+++ models/User.js
@@ -58,7 +58,7 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
     if(err) return cb(err)
     cb(null, isMatch)
   })
-}  
+}
 
 userSchema.methods.generateToken = function(cb) {
   var user = this;
@@ -74,6 +74,24 @@ userSchema.methods.generateToken = function(cb) {
     cb(null, user)
   })
 }
+
+
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  //user._id + '' = token
+  //토큰 디코드함
+  jwt.verify(token, 'secretToken', function(err, decoded) {
+    //유저 아이디로 유저 찾고 클라이언트에서 가져온 토큰과 DB에 보관된 토큰 일치하는지 확인
+    user.findOne({"_id": decoded, "token":token}, function (err, user) {
+      if(err) return cb(err);
+      cb(null, user)
+    })
+  })
+}
+
+
 
 const User = mongoose.model('User',userSchema)
 
